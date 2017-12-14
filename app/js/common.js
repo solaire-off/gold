7a302af1781f0ba3e1f5cd8086691c8cdea66c2d
solaire-off <victor374288@mail.ru>
2017-12-14 09:25:43 +0300

    Change colors and images, add more info, fix scroll anchor

--- app/js/common.js
+++ app/js/common.js
@@ -4,6 +4,8 @@ $(function() {
 
 	$( ".cross" ).hide();
 	$( ".menu-mobile").css('display',"none");
+
+
 	$( ".hamburger" ).click(function() {
 		$( ".menu-mobile" ).slideToggle( "slow", function() {
 			$( ".hamburger" ).hide();
@@ -26,12 +28,17 @@ $(function() {
 	});
 
 
-	$('.nav a, .menu-mobile a ').click(function (event) {
+	$('.nav a, .menu-mobile a, .anchor ').click(function (event) {
 		event.preventDefault();
-
+		
+		if (mobileOffset === 0) mobileOffset = -30;
+		
 		elementClick = $(this).attr("href");
+		
 		destination = $(elementClick).position().top - mobileOffset;
+		
 		$('html,body').stop().animate( { scrollTop: destination }, 1100 );
+		
 		mobileOffset = -30;
 
 
